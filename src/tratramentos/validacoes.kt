929db758d72e64e99a3068b4faa935b9d30a41b3
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2024-03-26 18:32:55 -0300

    validarPalpite()

--- src/tratramentos/validacoes.kt
+++ src/tratramentos/validacoes.kt
@@ -1,5 +1,6 @@
 package tratramentos
 
+import modelos.Carta
 import java.util.Scanner
 
 val leitura = Scanner(System.`in`)
@@ -11,4 +12,32 @@ fun validarQuantidadeCartas(quantidadeCartas : String) : Int {
     }
 
     return  quantidadeCartas.toInt()
+}
+
+
+fun validarPalpite(palpite : String) : Boolean {
+
+    val cores = Carta.cores
+    val numeros = Carta.numeros
+
+    val corValida = cores.any {
+        palpite.contains(it, ignoreCase = true)
+    }
+
+    val numeroValido = numeros.any {
+        palpite.contains(it.toString())
+    }
+
+    while (!corValida || !numeroValido) {
+        println("Palpite inválido. Informe palpite no formato/sequência 'cor numero' (Ex: azul 1):")
+        return validarPalpite(leitura.nextLine())
+    }
+
+    /* Como seria validação com regex:
+    fun validarPalpite(palpite: String): Boolean {
+        val formatoCorNumero = Regex("""^\s*(preto|azul|vermelho)\s+\d{1,2}\s*$""")
+        return formatoCorNumero.matches(palpite.trim())
+    }*/
+
+    return true
 }
